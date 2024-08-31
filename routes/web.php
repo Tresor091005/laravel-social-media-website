412458ebda5bf8c415b731db48906bd1f3372a3c
LAHAN Tresor <haileystone0856@gmail.com>
2024-08-31 17:01:45 +0100

    Implement user profile page UI

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('dashboard', function () {
+Route::get('/dashboard', function () {
     return to_route('home');
 })->name('dashboard');
 
@@ -12,8 +12,10 @@ Route::get('/',[HomeController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('home');
 
+Route::get('/u/{user:username}',[ProfileController::class, 'index'])
+    ->name('profile');
+
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
